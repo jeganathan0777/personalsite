7b6aef51dc486f5c6eaf5c4c9984a851f2653503
jeganathan0777 <108049034+jeganathan0777@users.noreply.github.com>
2024-08-24 08:18:58 +0530

    Update script.js

--- script.js
+++ script.js
@@ -30,34 +30,3 @@ document.addEventListener("DOMContentLoaded", () => {
     
 });
 
-document.addEventListener("DOMContentLoaded", () => {
-    const changingText = document.querySelector('.changing-text');
-    const phrases = ['JEGANATHAN', 'an Engineering Student', 'a Software Developer'];
-    let currentIndex = 0;
-    let charIndex = 0;
-    let isDeleting = false;
-
-    function type() {
-        const currentPhrase = phrases[currentIndex];
-        const displayedText = isDeleting 
-            ? currentPhrase.substring(0, charIndex--)
-            : currentPhrase.substring(0, charIndex++);
-
-        changingText.textContent = displayedText;
-
-        if (!isDeleting && charIndex === currentPhrase.length) {
-            setTimeout(() => isDeleting = true, 3000);
-        } else if (isDeleting && charIndex === 0) {
-            isDeleting = false;
-            currentIndex = (currentIndex + 1) % phrases.length;
-        }
-
-        const timeout = isDeleting ? 100 : 200;
-        setTimeout(type, timeout);
-    }
-
-    type();
-
-    
-});
-
